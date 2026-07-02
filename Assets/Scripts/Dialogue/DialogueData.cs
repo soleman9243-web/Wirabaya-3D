128412b5f3620b5cee7992bba6ceff28895d787a
Omnivor4 <farrelwahyu08@gmail.com>
2026-07-02 09:04:46 +0700

    NPLKNTL
    ckdw

--- Assets/Scripts/Dialogue/DialogueData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue System/Dialogue Data")]
-public class DialogueData : ScriptableObject
-{
-    [Tooltip("List of all dialogue nodes. The dialogue starts at index 0.")]
-    public List<DialogueNode> nodes = new List<DialogueNode>();
-}
--- Assets/Scripts/Quest/DataScript/QuestInstance.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-public class QuestInstance
-{
-    public QuestData data;
-    public bool isCompleted;
-
-    public List<ObjectiveProgress> objectives = new List<ObjectiveProgress>();
-}
